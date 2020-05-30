877e6aa5e1c380b010c7587709062557ed083f73
JacobGeoGeek <jacobchaar@live.ca>
2020-05-30 11:18:51 -0400

    create the folder TP1

---
+++ TP1/question1/monapp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+function afficherMenuPrincipale {
+    echo "Menu Principale"
+    echo "#-Disque"
+    echo "$-Système"
+    echo "&-Rèseau"
+    echo "Z-Quitter"
+}
+
+
+
+#************************************************************* 
+# Fonction : 
+#  Objectif : Script principal du questionnaire 
+# Notes  :  
+#************************************************************* 
+clear
+afficherMenuPrincipale
+
